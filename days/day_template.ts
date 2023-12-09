695d987fd7dfc2dd6c03cabd5e707ab24c35eaf7
Amodio <pscamodio@gmail.com>
2023-12-09 19:51:27 +0000

    Add day template

---
+++ days/day_template.ts
@@ -0,0 +1,13 @@
+import { createSolver } from "../utils/aoc.ts";
+
+const DEMO_INPUT = ``;
+
+function solve1(input = DEMO_INPUT): string {
+    return input;
+}
+
+function solve2(input = DEMO_INPUT): string {
+    return input;
+}
+
+export const solve = createSolver(solve1, solve2);

85a44f1c02e9a8b152a6c23b06ce62ec9b7388be
Amodio <pscamodio@gmail.com>
2023-12-10 20:35:25 +0000

    Solve Day 10 Part 2 badly

--- days/day10.ts
+++ days/day10.ts
@@ -1,10 +1,15 @@
 import { createSolver } from "../utils/aoc.ts";
 
-const DEMO_INPUT = `..F7.
-|FJ|.
-SJ.L7
-|F--J
-LJ...
+const DEMO_INPUT = `FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
 `;
 
 type Index = {
@@ -32,6 +37,14 @@ class Grid {
 
     constructor(input: string) {
         this.lines = input.trim().split("\n");
+    }
+
+    get rows() {
+        return this.lines.length;
+    }
+
+    get cols() {
+        return this.lines[0].length;
     }
 
     find(letter: string): Index {
@@ -71,6 +84,7 @@ class Grid {
     validConnections(position: Index): Index[] {
         const connections = this.connections(position);
         return connections.filter((target) => {
+            if (!this.isInsideGrid(target)) return false;
             const targetConnections = this.connections(target);
             return targetConnections.find(isSameIndexAs(position)) !== undefined;
         });
@@ -102,6 +116,61 @@ class Grid {
         }
         throw new Error("Not Found");
     }
+
+    isInsideGrid(point: Index) : boolean {
+        return point.col >= 0 && point.col < this.cols && point.row >= 0 && point.row < this.rows;
+    }
+
+    rayCast(from: Index, loop: Index[]): number {
+        let toCheck = move(from, 0, 1);
+        let count = 0;
+        let maybeHit : Index[] | undefined = undefined;
+
+        const checkHit = () => {
+            if (!maybeHit) return;
+            const letters = maybeHit.map((index) => this.get(index));
+            if (
+                letters.includes("|") || 
+                letters.includes("S") || //works only on my input as the S is a vertical line
+                letters.includes("L") && letters.includes("7") ||
+                letters.includes("J") && letters.includes("F")
+                ) {
+                    count++;
+                }
+            maybeHit = undefined;
+        }
+
+        while (this.isInsideGrid(toCheck)) {
+            const loopIndex = loop.findIndex(isSameIndexAs(toCheck));
+            if (loopIndex !== -1) {
+                if (!maybeHit) {
+                    maybeHit = [toCheck];
+                } else {             
+                    const prev = loop[loopIndex - 1] ?? loop[loop.length - 1];
+                    const next = loop[loopIndex + 1] ?? loop[0];
+                    const sameHit = isSameIndex(prev, from) || isSameIndex(next, from);
+                    if (sameHit) {
+                        maybeHit.push(toCheck);
+                    } else {
+                        checkHit();
+                        maybeHit = [toCheck];
+                    }
+                }
+            } else {
+                checkHit();
+            }
+            from = toCheck;
+            toCheck = move(toCheck, 0, 1);
+        }
+        checkHit();
+        return count;
+    }
+
+    isInsideLoop(point: Index, loop: Index[]): boolean {
+        if (loop.find(isSameIndexAs(point))) return false;
+        const hits = this.rayCast(point, loop);
+        return hits % 2 !== 0;
+    }
 }
 
 
@@ -112,7 +181,18 @@ function solve1(input = DEMO_INPUT): string {
 }
 
 function solve2(input = DEMO_INPUT): string {
-    return input;
+    const grid = new Grid(input);
+    const loop = grid.findLoop();
+    let counter = 0;
+    for (let row = 0; row < grid.rows; ++row) {
+        for (let col = 0; col < grid.cols; ++col) {
+            const index = {row, col};
+            if (grid.isInsideLoop(index, loop)) {
+                counter++;
+            }
+        }
+    }
+    return counter.toString();
 }
 
 export const solve = createSolver(solve1, solve2);
